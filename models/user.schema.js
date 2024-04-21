7cd9c8ac8472ac480e741437e67e92b5e1f8f232
pinki2909 <159130883+pinki2909@users.noreply.github.com>
2024-04-21 07:03:56 +0000

    added user part 1

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,10 +1,33 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 const userSchema = mongoose.Schema(
     {
         name:{
             type : String,
             required : [true , "name is requird"],
             maxLength : [50 , "name must be less than 50"]
-        }
+        },
+        email:{
+            type : String,
+            required : [true , "email is requird"],
+           unique : true
+        },
+        password:{
+            type : String,
+            required : [true , "password is requird"],
+            minLength : [5 , "password must be atleast of 5 character"],
+            select : false
+        },
+        role:{
+            type : String,
+            enum : Object.values(AuthRoles),
+            default : AuthRoles.USER
+        },
+        forgotPasswordToken:String,
+        forgotPasswordExpiry : Date , 
+    },
+    {
+        timestamps : true
     }
-)
+);
+export default moongoose.model("user" ,userSchema)
