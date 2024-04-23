62c76df3325690091a07cbf268d94446fedbfd6b
pinki2909 <159130883+pinki2909@users.noreply.github.com>
2024-04-23 17:30:27 +0000

    added morgan

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(cookieParser())
+//cors help in accessing 
+//telling who requestiong which route-morgan
+app.use(morgan('tiny'))
+
 export default app
