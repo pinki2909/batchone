66d700c0e32a0ded8370cd88ef361f6030a7b3d7
pinki2909 <159130883+pinki2909@users.noreply.github.com>
2024-04-23 16:21:30 +0000

    product schema added

--- models/products.js
+++ models/products.js
@@ -3,7 +3,41 @@ const productSchema = new mongoose.Schema(
     {
         name:{
            type : String,
-           
+           required :[true , "requires name"],
+           trim : true,
+           maxLength :[120,"productname can be max 120 char"]
+        },
+        name:{
+            type : Number,
+            required :[true , "requires price"],
+            maxLength :[5,"productname cannot  be more than 5 "]
+        },
+        description :{
+            type : String,
+        },
+        photos:[
+            {
+                secure_url :{
+                    type: String,
+                    required : true
+                }
+            }
+        ],
+        stock:{
+            type : Number,
+            default :0
+        },
+        sold:{
+            type : Number,
+            default : 0
+        },
+        collectionId:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Collection"
         }
+    },
+    {
+        timestamps:true
     }
 );
+export default mongoose.model("product",productSchema);
