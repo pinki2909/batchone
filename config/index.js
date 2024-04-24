65fc893c68562a27cb86a52685ef45159d4971ec
pinki2909 <159130883+pinki2909@users.noreply.github.com>
2024-04-24 06:09:07 +0000

    mail utility

--- config/index.js
+++ config/index.js
@@ -3,7 +3,12 @@ dotenv.config()
 const config = {
     jwt_SECRET : process.env.jwt_SECRET,
     jwt_EXPIRY : process.env.jwt_EXPIRY|| "30d",
-    MONGODB_URL : process.env.MONGODB_URL
-    PORT : process.env.PORT
+    MONGODB_URL : process.env.MONGODB_URL,
+    PORT : process.env.PORT,
+    SMTP_MAIL_HOST : process.env.SMTP_MAIL_HOST,
+    SMTP_MAIL_PORT : process.env.SMTP_MAIL_PORT,
+    SMTP_MAIL_USERNAME : process.env.SMTP_MAIL_USERNAME,
+    SMTP_MAIL_PASSWORD : process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_EMAIL : process.env.SMTP_MAIL_EMAIL,
 }
 export default config
---
+++ utils/mailHelper.js
@@ -0,0 +1,14 @@
+import transporter from '../config/transporter.config'
+import config   from '../config/index'
+const mailHelper = async(options)=> {
+    const message = {
+        from : config.SMTP_MAIL_EMAIL,
+        to:options.email,
+        subject : options.subject,
+        text :options.text,
+    }
+    await transporter.sendMail(message)
+
+}
+
+export default mailHelper
